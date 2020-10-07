68689bb729fa578a8ce383ca3790c75689235d33
Sebastian Klasa <sebastianklasa1@gmail.com>
2020-10-07 15:43:21 +0200

    checked frontend

--- src/main/resources/static/js/apiaccess.js
+++ src/main/resources/static/js/apiaccess.js
@@ -2,8 +2,8 @@ var arr = [];
 var arrID = [];
 var projCount = 0;
 //endpoint
-//var endpointConst = "https://portfolio-app2.herokuapp.com/";
-var endpointConst = "http://localhost:8090/";
+var endpointConst = "https://portfolio-app2.herokuapp.com/";
+//var endpointConst = "http://localhost:8090/";
 var request;
 
 function getProjectTitles(){
